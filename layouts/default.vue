35879cfe6b90a881e7fdad65ce3a05af05b628f4
Serhii Petrash <serhiipetrash@gmail.com>
2022-12-01 13:12:45 +0200

    add tailwindcss and setup color, pages, font, icons

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-8">
+    <header class="wrapper flex justify-between">
+      <NuxtLink to="/">
+        <h1 class="text-secondary text-5xl">Adventure Store</h1>
+      </NuxtLink>
+      <NuxtLink to="/cart">
+        <span class="material-icons-outlined text-secondary text-5xl">shopping_bag</span>
+      </NuxtLink>
+    </header>
+
+    <div class="wrapper">
+      <slot />
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ pages/cart.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="text-white text-2xl text-center my-8">Your Basket</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
