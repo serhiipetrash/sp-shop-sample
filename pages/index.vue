237cc8be8625218bee7ac406d617d45920d57714
Serhii Petrash <serhiipetrash@gmail.com>
2022-12-01 20:44:03 +0200

    add Pinia to project

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">All products</h2>
+
+    <div class="grid grid-cols-2 gap-7">
+      <div v-for="product in products">
+        <ProductCard :product="product" />
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script setup>
+//fetch products
+const { data: products } = useFetch('http://localhost:4000/products')
 
 </script>
 
