d6e9bd7f0850300a1f57f64c2430f9dac8123b8a
Serhii Petrash <serhiipetrash@gmail.com>
2022-12-01 21:24:52 +0200

    get products from Basket

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">Your Basket</h2>
+
+    <div
+      v-if="cartStore.cart.length"
+      class="card text-white text-xl"
+    >
+    <!-- product list -->
+      <div
+        v-for="product in cartStore.cart"
+        class="flex gap-8 items-center"
+      >
+        <img :src="product.img" :alt="product.title">
+        <p class="text-white">{{product.title}}</p>
+        <p class="text-white">{{(product.price * product.quantity)}} coins.</p>
+      </div>
+    </div>
+
+    <div v-else>
+      <p class="text-white">There are no items in your basket.</p>
+    </div>
+
   </div>
 </template>
 
 <script setup>
+import { useCartStore } from '@/stores/cartStore';
+
+const cartStore = useCartStore()
 
 </script>
 
